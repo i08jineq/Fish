200c5de551d735efaea18d38688885eeb127bd76
Vongsomxai Vilayouth <darklord@Vongsomxais-MacBook-Pro.local>
2019-01-26 10:05:40 +0900

    created base sigleton

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Singleton.Init();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton
+{
+    public static Singleton instance;
+    public Canvas mainCanvas;
+
+    public Transform cameraRoot;
+    public Camera mainCamera;
+    public static void Init()
+    {
+        if(instance == null)
+        {
+            instance = new Singleton();
+        }
+    }
+
+    public Singleton()
+    {
+        mainCanvas = GameObject.FindObjectOfType<Canvas>();
+        mainCamera = GameObject.FindObjectOfType<Camera>();
+
+        GameObject cameraRootObject = new GameObject("Camera Root");
+        cameraRootObject.transform.SetPositionAndRotation(mainCamera.transform.position, mainCamera.transform.rotation);
+
+        cameraRoot = cameraRootObject.transform;
+        mainCamera.transform.SetParent(cameraRoot);
+    }
+}
