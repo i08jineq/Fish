3d53f6ebbd74d190b50d46da9f106d12a35b5d17
tokkq <tokkogames@gmail.com>
2019-01-26 12:59:55 +0900

    アタックオブジェクト追加

--- Assets/Scripts/AttackObject.cs
+++ Assets/Scripts/AttackObject.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class AttackObject : MonoBehaviour
 {
-    public void Init()
-    {
+    Pawn _ownerPawn;
+    float _damage;
 
+    public void Init(Pawn ownerPawn)
+    {
+        _ownerPawn = ownerPawn;
     }
 
-    protected virtual void Attack()
+    protected virtual void Attack(Pawn attackedPawn)
     {
-
+        attackedPawn.TakeDamage(_damage);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        var pawn = other.GetComponent<Pawn>();
+
+        if(pawn != null && pawn != _ownerPawn)
+        {
+            Attack(pawn);
+        }
     }
 }
