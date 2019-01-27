d0b0f6925aa333449b0f961802cee0fee7a6b543
koki0419 <azunyan40.19@gmail.com>
2019-01-27 13:24:21 +0900

    PowerUp_SpeesUpを実施

--- Assets/Scripts/PowerUp/SpeedUp.cs
+++ Assets/Scripts/PowerUp/SpeedUp.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "HPUp", menuName = "powerup/Speed Up")]
+[CreateAssetMenu(fileName = "SpeedUp", menuName = "powerup/Speed Up")]
 public class SpeedUp: PowerUp
 {
-    public int amount = 10;
+    public int amount = 5;
     public override void OnAddedToPawn(Pawn pawn)
     {
         pawn.pawnState.Speed += amount;
