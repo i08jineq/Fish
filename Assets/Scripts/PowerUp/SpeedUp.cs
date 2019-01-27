45d124415cbc2e21fda452c40e0bc5cda99258cb
koki0419 <azunyan40.19@gmail.com>
2019-01-27 13:39:49 +0900

    SpeedUpを5から3に修正

--- Assets/Scripts/PowerUp/SpeedUp.cs
+++ Assets/Scripts/PowerUp/SpeedUp.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpeedUp", menuName = "powerup/Speed Up")]
 public class SpeedUp: PowerUp
 {
-    public int amount = 5;
+    public int amount = 3;
     public override void OnAddedToPawn(Pawn pawn)
     {
         pawn.pawnState.Speed += amount;
